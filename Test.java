de760dca3d15f73c65cfea892efad50d170196ec
thebee99 <104721836+thebee99@users.noreply.github.com>
2023-05-30 15:14:13 +0900

    Update Test.java

--- Test.java
+++ Test.java
@@ -71,6 +71,48 @@ public class Calculator {
             case "LCM":
                 engine = new LCMEngine();
                 break;
+package edu.handong.csee.java.hw3;
+
+import edu.handong.csee.java.hw3.engines.Computable;
+import edu.handong.csee.java.hw3.engines.CubeVolEngine;
+import edu.handong.csee.java.hw3.engines.FactorialEngine;
+import edu.handong.csee.java.hw3.engines.FibonacciEngine;
+import edu.handong.csee.java.hw3.engines.GCDEngine;
+import edu.handong.csee.java.hw3.engines.LCMEngine;
+import edu.handong.csee.java.hw3.engines.MaxEngine;
+import edu.handong.csee.java.hw3.engines.MinEngine;
+import edu.handong.csee.java.hw3.engines.SQRTEngine;
+import edu.handong.csee.java.hw3.engines.SphereVolEngine;
+import edu.handong.csee.java.hw3.exceptions.InvalidCommandException;
+import edu.handong.csee.java.hw3.exceptions.OneInputException;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        Calculator myCalculator = new Calculator();
+        try {
+            myCalculator.run(args);
+        } catch (InvalidCommandException e) {
+            System.out.println(e.getMessage());
+        } catch (OneInputException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void run(String[] args) throws InvalidCommandException, OneInputException {
+        if (args.length == 0) {
+            throw new InvalidCommandException(" ");
+        }
+
+        String engineName = args[0].toUpperCase();
+
+        Computable engine = null;
+
+        switch (engineName) {
+            case "LCM":
+                engine = new LCMEngine();
+                break;
             case "GCD":
                 engine = new GCDEngine();
                 break;
@@ -97,23 +139,18 @@ public class Calculator {
                 break;
             default:
                 throw new InvalidCommandException(engineName);
-         
+
         }
         if (args.length <= 1) {
             throw new InvalidCommandException(" ");
 
         }
 
-        // engine.setInput(args);
-        // engine.compute();
         String[] inputs = new String[args.length - 1];
         System.arraycopy(args, 1, inputs, 0, inputs.length);
         engine.setInput(inputs);
         engine.compute();
 
-        System.out.println("The result of " +  engineName + " is " + engine.getResult() + ".");
-
+        System.out.println("The result of " + engineName + " is " + engine.getResult() + ".");
     }
-
-
 }
